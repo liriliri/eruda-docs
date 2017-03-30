b905737f015acacad1b368e1a0739b6a6960e06d
surunzi <surunzi@foxmail.com>
2017-03-30 13:24:40 +0800

    Dev: Wrap js inside function for minification

--- eustia.js
+++ eustia.js
@@ -1846,6 +1846,50 @@ window._ = (function()
         return exports;
     })({});
 
+    /* ------------------------------ ready ------------------------------ */
+
+    _.ready = (function ()
+    {
+        /* Invoke callback when dom is ready, similar to jQuery ready.
+         *
+         * |Name|Type    |Desc             |
+         * |----|--------|-----------------|
+         * |fn  |function|Callback function|
+         *
+         * ```javascript
+         * ready(function ()
+         * {
+         *     // It's safe to manipulate dom here.
+         * });
+         * ```
+         */
+
+        var fns = [],
+            listener,
+            doc = document,
+            hack = doc.documentElement.doScroll,
+            domContentLoaded = 'DOMContentLoaded',
+            loaded = (hack ? /^loaded|^c/ : /^loaded|^i|^c/).test(doc.readyState);
+
+        if (!loaded)
+        {
+            doc.addEventListener(domContentLoaded, listener = function ()
+            {
+                doc.removeEventListener(domContentLoaded, listener);
+                loaded = 1;
+                /* eslint-disable no-cond-assign */
+                while (listener = fns.shift()) listener();
+            });
+        }
+
+        function exports(fn)
+        {
+            loaded ? setTimeout(fn, 0) : fns.push(fn)
+        }
+
+        return exports;
+    })();
+
     /* ------------------------------ rtrim ------------------------------ */
 
     var rtrim = _.rtrim = (function ()
