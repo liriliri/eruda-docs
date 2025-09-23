5b6d2b7572b92e651676c570af82815e77e5a1f3
redhoodsu <redhoodsu@tencent.com>
2025-09-23 12:12:59 +0800

    chore: small changes

--- docs/.vitepress/theme/components/Home.vue
+++ docs/.vitepress/theme/components/Home.vue
@@ -15,145 +15,90 @@ const LunaShaderToyPlayer = defineClientComponent(
   ]
 )
 
-// https://www.shadertoy.com/view/wtcXWr
-const code = `#define S(a, b, t) smoothstep(a, b, t)
+// https://www.shadertoy.com/view/cdj3DW
+const code = `const int MAX_ITER = 18;
 
-float distLine(vec2 p, vec2 a, vec2 b) {
-  vec2 pa = p - a;
-  vec2 ba = b - a;
-  float t = clamp(dot(pa, ba) / dot(ba, ba), 0.0, 1.0);
-  return length(pa - ba * t);
-}
-
-float line(vec2 p, vec2 a, vec2 b) {
-  float d = distLine(p, a, b);
-  float m = S(0.03, 0.01, d);
-  float d2 = length(a - b);
-  m *= S(1.2, 0.8, d2) * 0.5 + S(0.05, 0.03, abs(d2 - 0.75));
-  return m;
-}
-
-float distTriangle(in vec2 p, in vec2 p0, in vec2 p1, in vec2 p2)
+float field(vec3 p, float s, int iter)
 {
-  vec2 e0 = p1 - p0;
-  vec2 e1 = p2 - p1;
-  vec2 e2 = p0 - p2;
-
-  vec2 v0 = p - p0;
-  vec2 v1 = p - p1;
-  vec2 v2 = p - p2;
-
-  vec2 pq0 = v0 - e0 * clamp(dot(v0, e0) / dot(e0, e0), 0.0, 1.0);
-  vec2 pq1 = v1 - e1 * clamp(dot(v1, e1) / dot(e1, e1), 0.0, 1.0);
-  vec2 pq2 = v2 - e2 * clamp(dot(v2, e2) / dot(e2, e2), 0.0, 1.0);
-
-  float s = sign(e0.x * e2.y - e0.y * e2.x);
-  vec2 d = min(min(vec2(dot(pq0, pq0), s * (v0.x * e0.y - v0.y * e0.x)),
-    vec2(dot(pq1, pq1), s * (v1.x * e1.y - v1.y * e1.x))),
-    vec2(dot(pq2, pq2), s * (v2.x * e2.y - v2.y * e2.x)));
-
-  return -sqrt(d.x) * sign(d.y);
-}
-
-float triangle(vec2 p, vec2 a, vec2 b, vec2 c) {
-  float d = distTriangle(p, a, b, c);
-  float m = S(0.03, 0.01, d);
-  float d2 = length(a - b);
-  m *= S(1.2, 0.8, d2) * 0.5 + S(0.05, 0.03, abs(d2 - 0.75));
-  return m;
-}
-
-float N21(vec2 p) {
-  p = fract(p * vec2(233.34, 851.73));
-  p += dot(p, p + 23.45);
-  return fract(p.x * p.y);
-}
-
-vec2 N22(vec2 p) {
-  float n = N21(p);
-  return vec2(n, N21(p + n));
-}
-
-vec2 getPos(vec2 id, vec2 offset) {
-  vec2 n = N22(id + offset) * iTime;
-  return offset + sin(n) * 0.4;
-}
-
-float layer(vec2 uv) {
-  vec2 gv = fract(uv) - 0.5;
-  vec2 id = floor(uv);
-
-  vec2 p[9];
-  int i = 0;
-  for (float y = -1.0; y <= 1.0; y++) {
-    for (float x = -1.0; x <= 1.0; x++) {
-      p[i++] = getPos(id, vec2(x, y));
+  float accum = s / 4.0;
+  float prev = 0.0;
+  float tw = 0.0;
+  for (int i = 0; i < MAX_ITER; ++i)
+  {
+    if (i >= iter) // drop from the loop if the number of iterations has been completed - workaround for GLSL loop index limitation
+    {
+      break;
     }
+    float mag = dot(p, p);
+    p = abs(p) / mag + vec3(-0.5, -0.4, -1.487);
+    float w = exp(-float(i) / 5.0);
+    accum += w * exp(-9.025 * pow(abs(mag - prev), 2.2));
+    tw += w;
+    prev = mag;
   }
+  return max(0.0, 5.2 * accum / tw - 0.65);
+}
 
-  float t = iTime * 10.0;
-  float m = 0.0;
-  for (int i = 0; i < 9; i++) {
-    m += line(gv, p[4], p[i]);
+vec3 nrand3(vec2 co)
+{
+  vec3 a = fract(cos(co.x * 8.3e-3 + co.y) * vec3(1.3e5, 4.7e5, 2.9e5));
+  vec3 b = fract(sin(co.x * 0.3e-3 + co.y) * vec3(8.1e5, 1.0e5, 0.1e5));
+  vec3 c = mix(a, b, 0.5);
+  return c;
+}
 
-    vec2 j = (p[i] - gv) * 20.0;
-    float sparkle = 1.0 / dot(j, j);
-
-    m += sparkle * (sin(t + fract(p[i].x) * 10.0) * 0.5 + 0.5);
-
-    for (int yi = i + 1; yi < 9; yi++) {
-      for (int zi = yi + 1; zi < 9; zi++) {
-        float len1 = abs(length(p[i] - p[yi]));
-        float len2 = abs(length(p[yi] - p[zi]));
-        float len3 = abs(length(p[i] - p[zi]));
-
-        if ((len1 + len2 + len3) < 2.8) {
-          m += triangle(gv, p[i], p[yi], p[zi]) * 0.8;
-        }
-      }
-    }
-  }
-  m += line(gv, p[1], p[3]);
-  m += line(gv, p[1], p[5]);
-  m += line(gv, p[7], p[3]);
-  m += line(gv, p[7], p[5]);
-
-  return m;
+vec4 starLayer(vec2 p, float time)
+{
+  vec2 seed = 1.9 * p.xy;
+  seed = floor(seed * max(iResolution.x, 600.0) / 1.5);
+  vec3 rnd = nrand3(seed);
+  vec4 col = vec4(pow(rnd.y, 17.0));
+  float mul = 10.0 * rnd.x;
+  col.xyz *= sin(time * mul + mul) * 0.25 + 1.0;
+  return col;
 }
 
 void mainImage(out vec4 fragColor, in vec2 fragCoord)
 {
-  vec2 uv = (fragCoord - 0.5 * iResolution.xy) / iResolution.y;
+  float time = iTime / (iResolution.x / 1000.0);
 
-  float m = 0.0;
-  float t = iTime * 0.1;
+  // first layer of the kaliset fractal
+  vec2 uv = 2.0 * fragCoord / iResolution.xy - 1.0;
+  vec2 uvs = uv * iResolution.xy / max(iResolution.x, iResolution.y);
+  vec3 p = vec3(uvs / 2.5, 0.0) + vec3(0.8, -1.3, 0.0);
+  p += 0.45 * vec3(sin(time / 32.0), sin(time / 24.0), sin(time / 64.0));
 
-  float gradient = uv.y;
+  // adjust first layer position based on mouse movement
+  p.x += mix(-0.02, 0.02, (iMouse.x / iResolution.x));
+  p.y += mix(-0.02, 0.02, (iMouse.y / iResolution.y));
 
-  float s = sin(t);
-  float c = cos(t);
-  mat2 rot = mat2(c, -s, s, c);
-  uv *= rot;
+  float freqs[4];
+  freqs[0] = 0.45;
+  freqs[1] = 0.4;
+  freqs[2] = 0.15;
+  freqs[3] = 0.9;
 
-  for (float i = 0.0; i < 1.0; i += 1.0 / 4.0) {
-    float z = fract(i + t);
-    float size = mix(10.0, 0.5, z);
-    float fade = S(0.0, 0.5, z) * S(1.0, 0.8, z);
+  float t = field(p, freqs[2], 13);
+  float v = (1.0 - exp((abs(uv.x) - 1.0) * 6.0)) * (1.0 - exp((abs(uv.y) - 1.0) * 6.0));
 
-    m += layer(uv * size + i * 20.0) * fade;
-  }
+  // second layer of the kaliset fractal
+  vec3 p2 = vec3(uvs / (4.0 + sin(time * 0.11) * 0.2 + 0.2 + sin(time * 0.15) * 0.3 + 0.4), 4.0) + vec3(2.0, -1.3, -1.0);
+  p2 += 0.16 * vec3(sin(time / 32.0), sin(time / 24.0), sin(time / 64.0));
 
-  vec3 base = sin(t * 5.0 * vec3(0.345, 0.456, 0.567)) * 0.4 + 0.6;
-  vec3 col = m * base;
+  // adjust second layer position based on mouse movement
+  p2.x += mix(-0.01, 0.01, (iMouse.x / iResolution.x));
+  p2.y += mix(-0.01, 0.01, (iMouse.y / iResolution.y));
+  float t2 = field(p2, freqs[3], 18);
+  vec4 c2 = mix(0.5, 0.2, v) * vec4(5.5 * t2 * t2 * t2, 2.1 * t2 * t2, 2.2 * t2 * freqs[0], t2);
 
-  col -= gradient * base;
+  // add stars (source: https://glslsandbox.com/e#6904.0)
+  vec4 starColour = vec4(0.0);
+  starColour += starLayer(p.xy, time); // add first layer of stars
+  starColour += starLayer(p2.xy, time); // add second layer of stars
 
-  //vec2 gv = fract(uv) - 0.5;
-  //if(gv.x > 0.48 || gv.y > 0.48){
-  //	col = vec3(1.0, 0.0, 0.0);
-  //}
-
-  fragColor = vec4(col, 1.0);
+  const float brightness = 1.0;
+  vec4 colour = mix(freqs[3] - 0.3, 1.0, v) * vec4(1.5 * freqs[2] * t * t * t, 1.2 * freqs[1] * t * t, freqs[3] * t, 1.0) + c2 + starColour;
+  fragColor = vec4(brightness * colour.xyz, 1.0);
 }`
 
 const renderPass = ref([
